f44fcbdaba8013447a9705f721fd1fcfa5d19c6f
aplaco <lyon108@naver.com>
2025-04-08 11:36:08 +0900

    feat: 대입, 산술, 복합대입 연산자 개념

--- main.js
+++ main.js
@@ -1,38 +1,33 @@
 /*
-  자료형 : 자바스크립트에 다루는 자료의 종류
+  연산자: 특정 값을 도출하기 위한 식별자 
 
-  원시형 자료 (primitive type) : 메모리 안에 실제 값이 담기는 자료형 
-  - 문자열
-  - 숫자
-  - boolean
-
-  참조형 자료 (reference type) : 메모리에 실제 값이 아닌 값의 위치값이 담기는 자료형
-  - 배열
-  - 객체
+  대입 연산자
+  산술 연산자
+  복합 대입 연산자
+  비교 연산자
 */
 
-let myName = "Davide"; //문자열 (string)
-let myAge = 40; //숫자 (number)
-let isFemale = false; //논리형 (boolean)
+//대입 연산자
+let myName = "David"; //=을 기준으로 오른쪽 값이 먼저 해석되서 왼쪽에 대입
 
-let myColors = ["red", "green", "blue"]; //배열 (array)
-//객체 (Object)
-//복잡한 구조의 데이터를 그룹화 할때
-let myInfo = {
-  name: "David",
-  age: 40,
-  isFemale: false,
-};
+//산술 연산자
+let result;
+result = 2 + 3;
+console.log("+", result);
 
-//객체의 구조를 활용한 개발 방식 :객체지향 프로그래밍 (OOP: Object Oriendted Programing)
+result = 2 - 3;
+console.log("-", result);
 
-//배열값 추출 : 배열명[순번]
-console.log(myColors[0]);
+result = 2 * 3;
+console.log("*", result);
 
-//객체값 추출 : 객체명.프로퍼티명
-console.log(myInfo.name);
+result = 2 / 3;
+console.log("/", result);
 
-/*
-   undefined : 있어야될 값이 없을때 (에러 상황)
-   null : 일부러 값을 초기에 비워둘떄 (에러 아님)
- */
+result = 5 % 2; //몫이 아닌 나머지 구하기
+console.log("%", result);
+
+let num = 0;
+//num = num + 1;
+//num += 1;
+num++;

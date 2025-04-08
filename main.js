aa91c8d7396ff63312a9571002024680a49db6c2
aplaco <lyon108@naver.com>
2025-04-08 16:28:51 +0900

    feat: 이벤트 발생시 DOM정보 제어

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-/*
-  DOM스크립트
-
-  DOM (Document Object Model)
-  -html태그를 브라우저가 해석해서 자바스크립트가 제어 가능한 형태로 변환된 객체
-
-  DOM선택
-  DOM 정보 출력
-  DOM 텍스트 정보 변경
-  DOM 스타일 정보 변경
-  DOM 속성 정보 변경
-  DOM에 이벤트 연결
-*/
-
-//h1태그 요소를 돔스크립트로 선택
 const h1 = document.querySelector("h1");
 const link = document.querySelector("a");
-console.log(h1);
-console.dir(h1);
-console.dir(link);
+// const btn1 = document.querySelector(".btn1");
+// const btn2 = document.querySelector(".btn2");
+// const btn3 = document.querySelector("btn3");
+const btns = document.querySelectorAll("button");
+console.log(btns); //NodeList형태로 반환됨 (유사배열: 배열과 비슷한 데이터 그룹)
 
-//DOM의 텍스트 변경
-//h1.innerText = "김영희";
+//btn1클릭시 h1의 텍스트 변경
+btns[0].addEventListener("click", () => {
+  h1.innerText = "김영희";
+});
 
-//DOM의 html 구조 변경
-//h1.innerHTML = "<span>김판섭</span>";
+//스타일 변경
+btns[1].addEventListener("click", () => {
+  h1.style.color = "aqua";
+});
 
-//DOM스타일 변경
-h1.style.color = "aqua";
-
-//DOM 속성 변경
-link.href = "https://www.nate.com";
-link.innerText = "네이트";
+//속성 변경
+btns[2].addEventListener("click", () => {
+  link.href = "https://www.nate.com";
+  link.innerText = "네이트";
+});

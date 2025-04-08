7285553009f6128639bc5d1a0222fba52f48eb83
aplaco <lyon108@naver.com>
2025-04-08 16:42:49 +0900

    feat: 자바스크립트로 웹 모션 구현하는 예시

---
+++ main.js
@@ -0,0 +1,11 @@
+const box = document.querySelector(".box");
+const btns = document.querySelectorAll("button");
+let count = 0;
+
+btns[0].addEventListener("click", () => {
+  box.style.transform = `rotate(${30 * --count}deg)`;
+});
+
+btns[1].addEventListener("click", () => {
+  box.style.transform = `rotate(${30 * ++count}deg)`;
+});

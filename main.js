b68f3b8d0714c4ca78c83c50e9e6ad2d00f9dca0
aplaco <lyon108@naver.com>
2025-04-08 14:34:04 +0900

    feat: forEach를 통한 반복처리

---
+++ main.js
@@ -0,0 +1,35 @@
+/*
+  배열과 반복문
+
+  배열 : 비슷한 성격의 복수개의 데이터를 그룹핑하기 위한 자료형
+  배열의 데이터를 손쉽게 제어하기 위한 문법 반복문
+
+  for(let i=0; i<최대수치; i++){
+    반복할 코드 입력
+  }
+  i값이 0부터 최대수치값 만큼 {}안쪽의 구문을 반복처리
+
+  배열의 갯수 구하는 법
+  - 배열명.length
+
+  배열 반복처리 내장 함수
+  - 직접 정의하지 않더라도 자주쓰는 기능등을 묶어서 미리 등록한 함수
+
+  배열.forEach(익명함수)
+*/
+
+const colors = ["red", "green", "blue", "orange"];
+console.log(colors.length);
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+colors.forEach((data, index) => {
+  console.log(data);
+  console.log(index);
+  console.log("---------");
+});

b2b432c18b1db1df8881f402c2727c3a5798aeb4
aplaco <lyon108@naver.com>
2025-04-08 11:43:29 +0900

    feat: 비교연산자의 이해

--- main.js
+++ main.js
@@ -31,3 +31,16 @@ let num = 0;
 //num = num + 1;
 //num += 1;
 num++;
+
+//비교 연산자: 복수개의 값을 서로 비교 : true, false값을 얻기 위함
+
+let number = 1;
+let text = "1";
+
+//console.log(number === text); //값과 자료형 모두 비교
+//console.log(number == text); //값만 비교
+
+console.log(true && true); //2개 모두 참이여야지만 true
+console.log(true && false); //&&일때 하나라도 참이 아니면 false
+console.log(true || false); //|| 둘중에 하나만 참이여도 true 
+

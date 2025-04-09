d54362cb58ae4ee6afef38694c5e2fb47123e0ae
aplaco <lyon108@naver.com>
2025-04-09 09:46:52 +0900

    feat: css에 연결된 스타일 값을 js로 가져오는 방법

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-const btnCall = document.querySelector(".btnCall");
-const btnClose = document.querySelector(".btnClose");
-const mobileMenu = document.querySelector(".mobileMenu");
+const box = document.querySelector("article");
 
-//메뉴 활성화 처리 이벤트
-btnCall.addEventListener("click", () => {
-  mobileMenu.classList.add("on");
-});
+//box.style.backgroundColor = "hotpink";
+//box.style["background-color"] = "lightgreen";
 
-//메뉴 비활성화 처리 이벤트
-btnClose.addEventListener("click", () => {
-  mobileMenu.classList.remove("on");
-});
+// 자스는 html외의 css파일의 접근불가하므로
+// css에 있는 스타일 정보를 가져오는것이 아닌
+// 화면에 이미 렌더링된 결과화면을 다시 역으로 계산해서 가져옴
+const styleInfo = getComputedStyle(box);
+console.log(styleInfo.backgroundColor);
